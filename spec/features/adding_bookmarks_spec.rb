e22f71754d2cd07077b07af22bf9e74803ea4fce
Reeshul <reeshul@hotmail.com>
2021-01-27 17:54:59 +0000

    wrapping databse in objects

--- spec/features/adding_bookmarks_spec.rb
+++ spec/features/adding_bookmarks_spec.rb
@@ -3,10 +3,9 @@
 feature ' adding bookmark' do
   scenario ' whilst catching some waves on the world wide web with my buds' do
     visit('/bookmarks/add')
-    fill_in("url", with: 'http://testbookmark.com')
+    fill_in("url", with: 'http://www.testbookmark.com')
+    fill_in("title", with: 'Test Title')
     click_button('Submit')
-
-    expect(page).to have_content 'http://testbookmark.com'
-
+    expect(page).to have_link("Test Title", href: "http://www.testbookmark.com")
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -5,14 +5,14 @@ feature 'Viewing bookmarks' do
 
 
     # Add the test data
-    Bookmark.create(url: 'http://www.makersacademy.com')
-    Bookmark.create(url: 'http://www.destroyallsoftware.com')
-    Bookmark.create(url: 'http://www.google.com')
+    Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Bookmark.create(url: 'http://www.destroyallsoftware.com', title: 'Destroy All Software')
+    Bookmark.create(url: 'http://www.google.com', title: 'Gooooooogle')
 
     visit('/bookmarks')
 
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_link("Makers Academy", href: "http://www.makersacademy.com")
+    expect(page).to have_link("Destroy All Software", href: "http://www.destroyallsoftware.com")
+    expect(page).to have_link("Gooooooogle", href: "http://www.google.com")
   end
 end
